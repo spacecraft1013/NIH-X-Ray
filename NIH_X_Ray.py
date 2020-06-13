4ecb8de93d8cb36926f0bb4d8f3280f814f69bf1
Amin Boukari <ambouk3@gmail.com>
2020-06-13 02:59:10 -0400

    Testing with Autokeras instead of kerastuner, fail

--- NIH_X_Ray.py
+++ NIH_X_Ray.py
@@ -8,9 +8,10 @@ import create_data
 import sklearn as sk
 import tensorflow as tf
 import model
-import kerastuner as kt
+#import kerastuner as kt
+import autokeras as ak
 
-model_save_name = "model_adam"
+model_save_name = "model_ak"
 
 if os.path.exists(r"data/arrays/X_train_128.pickle") == False:
     create_data.main()
@@ -21,17 +22,20 @@ y_train = pickle.load(open(r"data/arrays/y_train_128.pickle", "rb"))
 X_test = pickle.load(open(r"data/arrays/X_test_128.pickle", "rb"))
 y_test = pickle.load(open(r"data/arrays/y_test_128.pickle", "rb"))
 
-model = model.model(128)
-print(model.summary())
+# model = model.model(128)
+# print(model.summary())
 
-tuner = kt.Hyperband(model, objective='val_accuracy', max_epochs=250, hyperband_iterations=5)
+#tuner = kt.Hyperband(model, objective='val_accuracy', max_epochs=250, hyperband_iterations=5)
 
-#model = keras.applications.inception_v3.InceptionV3(input_shape=(128, 128, 1), include_top=True, weights=None, classes=15)
-#model = keras.applications.mobilenet_v2.MobileNetV2(input_shape=(128, 128, 1), include_top=True, weights=None, classes=15)
-#model = keras.applications.vgg19.VGG19(input_shape=(128, 128, 1), include_top=True, weights=None, classes=15)
+model = ak.ImageClassifier(max_trials=10)
+model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=250)
 
-#print("Loading from previous checkpoint")
-#model.load_weights("data/checkpoints/.ckpt")
+# model = keras.applications.inception_v3.InceptionV3(input_shape=(128, 128, 1), include_top=True, weights=None, classes=15)
+# model = keras.applications.mobilenet_v2.MobileNetV2(input_shape=(128, 128, 1), include_top=True, weights=None, classes=15)
+# model = keras.applications.vgg19.VGG19(input_shape=(128, 128, 1), include_top=True, weights=None, classes=15)
+
+# print("Loading from previous checkpoint")
+# model.load_weights("data/checkpoints/.ckpt")
 
 # print("Computing sample weights...")
 # class_weights = sk.utils.class_weight.compute_sample_weight('balanced', y_train)
@@ -44,12 +48,12 @@ tuner = kt.Hyperband(model, objective='val_accuracy', max_epochs=250, hyperband_
 # cp_callback = keras.callbacks.ModelCheckpoint(filepath=checkpoint_path, save_weights_only=True, verbose=1)
 
 # model.fit(X_train, y_train, epochs=250, batch_size=64, validation_data=(X_test, y_test), class_weight=class_weights, callbacks=[cp_callback])
-tuner.search(X_train, y_train, epochs=250, validation_data=(X_test, y_test))
+# tuner.search(X_train, y_train, epochs=250, validation_data=(X_test, y_test))
 
-best_model = tuner.get_best_models(1)[0]
-print(tuner.get_best_hyperparameters(1)[0])
+# best_model = tuner.get_best_models(1)[0]
+# print(tuner.get_best_hyperparameters(1)[0])
 
-test_loss, test_acc, test_mae = best_model.evaluate(X_test, y_test)
+test_loss, test_acc = model.evaluate(X_test, y_test)
 print('\nTest Accuracy: {}\nTest Loss: {}\nTest MAE: {}'.format(test_acc, test_loss, test_mae))
 
-best_model.save('data/models/{}.h5'.format(model_save_name))
+#model.save('data/models/{}.h5'.format(model_save_name))
