a218c9a94b23d18273bc521f62328e022a2297f0
Amin Boukari <ambouk3@gmail.com>
2020-07-25 03:45:28 -0400

    Multi Label classification + slight model improvements

--- model.py
+++ model.py
@@ -5,20 +5,20 @@ def model_generator(hp):
 
     model.add(keras.layers.Input(shape=(128, 128, 1)))
     
-    for i in range(hp.Int('3blocks', min_value=1, max_value=150, step=1)):
+    for i in range(hp.Int('3blocks', min_value=0, max_value=150, step=1)):
         model.add(keras.layers.Conv2D(filters=hp.Int('3block_conv_filter', min_value=32, max_value=128, step=1), kernel_size=hp.Choice('3block_conv_kernel', values = [3,5]), activation='relu', padding='same'))
         model.add(keras.layers.Conv2D(filters=hp.Int('3block_conv_filter', min_value=32, max_value=128, step=1), kernel_size=hp.Choice('3block_conv_kernel', values = [3,5]), activation='relu', padding='same'))
         model.add(keras.layers.Conv2D(filters=hp.Int('3block_conv_filter', min_value=32, max_value=128, step=1), kernel_size=hp.Choice('3block_conv_kernel', values = [3,5]), activation='relu', padding='same'))
         model.add(keras.layers.MaxPooling2D(hp.Int('3block_pool_size', min_value=2, max_value=10, step=1), padding='same'))
 
-    for i in range(hp.Int('4blocks', min_value=1, max_value=150, step=1)):
+    for i in range(hp.Int('4blocks', min_value=0, max_value=150, step=1)):
         model.add(keras.layers.Conv2D(filters=hp.Int('4block_conv_filter', min_value=32, max_value=128, step=1), kernel_size=hp.Choice('4block_conv_kernel', values = [3,5]), activation='relu', padding='same'))
         model.add(keras.layers.Conv2D(filters=hp.Int('4block_conv_filter', min_value=32, max_value=128, step=1), kernel_size=hp.Choice('4block_conv_kernel', values = [3,5]), activation='relu', padding='same'))
         model.add(keras.layers.Conv2D(filters=hp.Int('4block_conv_filter', min_value=32, max_value=128, step=1), kernel_size=hp.Choice('4block_conv_kernel', values = [3,5]), activation='relu', padding='same'))
         model.add(keras.layers.Conv2D(filters=hp.Int('4block_conv_filter', min_value=32, max_value=128, step=1), kernel_size=hp.Choice('4block_conv_kernel', values = [3,5]), activation='relu', padding='same'))
         model.add(keras.layers.MaxPooling2D(hp.Int('4block_pool_size', min_value=2, max_value=10, step=1), padding='same'))
 
-    for i in range(hp.Int('5blocks', min_value=1, max_value=150, step=1)):
+    for i in range(hp.Int('5blocks', min_value=0, max_value=150, step=1)):
         model.add(keras.layers.Conv2D(filters=hp.Int('5block_conv_filter', min_value=32, max_value=128, step=1), kernel_size=hp.Choice('5block_conv_kernel', values = [3,5]), activation='relu', padding='same'))
         model.add(keras.layers.Conv2D(filters=hp.Int('5block_conv_filter', min_value=32, max_value=128, step=1), kernel_size=hp.Choice('5block_conv_kernel', values = [3,5]), activation='relu', padding='same'))
         model.add(keras.layers.Conv2D(filters=hp.Int('5block_conv_filter', min_value=32, max_value=128, step=1), kernel_size=hp.Choice('5block_conv_kernel', values = [3,5]), activation='relu', padding='same'))
