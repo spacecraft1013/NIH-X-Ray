ffc8a1c8f0b2b6402a76828c4c4e4cd472db128e
Amin Boukari <ambouk3@gmail.com>
2020-10-10 14:41:49 -0400

    Bug fixes

--- NIH_X_Ray.py
+++ NIH_X_Ray.py
@@ -3,22 +3,22 @@ import numpy as np
 import keras
 import datetime
 import os
-import multithreaded_preprocessing as preprocessing
-import sklearn as sk
-import tensorflow as tf
-from model import model_generator
-import kerastuner as kt
+from multithreaded_preprocessing import PreprocessImages
 
 model_save_name = "densenet201_kld"
+epochs = 250
+image_size = 256
+batch_size = 32
 
 if os.path.exists("data/arrays/X_train_256.npy") == False:
-    preprocessing.main()
+    preprocessor = PreprocessImages("F:/Datasets/NIH X-Rays/data", image_size=image_size)
+    preprocessor()
 
 print("Importing Arrays")
-X_train = np.load(open("data/arrays/X_train_256.npy", "rb"))
-y_train = np.load(open("data/arrays/y_train_256.npy", "rb"))
-X_test = np.load(open("data/arrays/X_test_256.npy", "rb"))
-y_test = np.load(open("data/arrays/y_test_256.npy", "rb"))
+X_train = np.load(open(f"data/arrays/X_train_{image_size}.npy", "rb"))
+y_train = np.load(open(f"data/arrays/y_train_{image_size}.npy", "rb"))
+X_test = np.load(open(f"data/arrays/X_test_{image_size}.npy", "rb"))
+y_test = np.load(open(f"data/arrays/y_test_{image_size}.npy", "rb"))
 
 # tuner = kt.BayesianOptimization(model_generator, objective='val_accuracy', max_trials=500, project_name="NIH X-Ray Model")
 
@@ -40,7 +40,7 @@ checkpoint_dir = os.path.dirname(checkpoint_path)
 densenet.save_weights(checkpoint_path.format(epoch=0))
 cp_callback = keras.callbacks.ModelCheckpoint(filepath=checkpoint_path, save_weights_only=True, verbose=1)
 
-densenet.fit(X_train, y_train, batch_size=32, epochs=250, validation_data=(X_test, y_test), callbacks=[tensorboard_callback, cp_callback])
+densenet.fit(X_train, y_train, batch_size=32, epochs=epochs, validation_data=(X_test, y_test), callbacks=[tensorboard_callback, cp_callback])
 
 test_loss, test_acc = densenet.evaluate(X_test, y_test)
 print(f'\nTest Accuracy: {test_acc}\nTest Loss: {test_loss}')
