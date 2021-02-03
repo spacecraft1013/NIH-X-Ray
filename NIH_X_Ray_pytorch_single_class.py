61d57e327afb53a1c47ee4ab7b27d777f606b66e
Amin Boukari <ambouk3@gmail.com>
2021-02-03 17:03:31 -0500

    Added support for single class training

---
+++ NIH_X_Ray_pytorch_single_class.py
@@ -0,0 +1,245 @@
+import argparse
+import copy
+import os
+import time
+
+import numpy as np
+import torch
+import torch.nn as nn
+from torch.cuda.amp import GradScaler, autocast
+from torch.optim import Adamax, lr_scheduler
+from torch.utils.data import DataLoader, TensorDataset, random_split
+from torch.utils.tensorboard import SummaryWriter
+from tqdm import tqdm
+
+from multithreaded_preprocessing import PreprocessImages
+
+parser = argparse.ArgumentParser()
+parser.add_argument('class_name', type=str,
+                    help='Class to train on')
+parser.add_argument('-c', '--checkpoint', default=None, type=str,
+                    help='Checkpoint file to load from')
+parser.add_argument('-e', '--epochs', default=250, type=int,
+                    help='Number of epochs to use')
+parser.add_argument('--no-pin-mem', default=False, action='store_true',
+                    help="Don't use pinned memory")
+parser.add_argument('--name', default=None, type=str,
+                    help='Name to save model (no file extension)')
+parser.add_argument('--checkpoint-dir', default=None, type=str,
+                    help='Checkpoint directory')
+parser.add_argument('--img-size', default=256, type=int,
+                    help='Single sided image resolution')
+parser.add_argument('--batch-size', default=32, type=int,
+                    help='Batch size to use for training')
+parser.add_argument('-s', '--seed', default=0, type=int,
+                    help='Seed to use for random values')
+parser.add_argument('--device', default=0, type=int,
+                    help='GPU ID to train on')
+args = parser.parse_args()
+
+args.pin_mem = not args.no_pin_mem
+
+starttime = time.time()
+
+torch.manual_seed(args.seed)
+
+if not args.name:
+    args.name = "binary-" + args.class_name
+
+if not args.checkpoint_dir:
+    args.checkpoint_dir = f"data/checkpoints/{args.name}-{int(starttime)}/"
+
+if not os.path.exists(args.checkpoint_dir):
+    os.mkdir(args.checkpoint_dir)
+
+print("Importing Arrays")
+if not os.path.exists(f"data/arrays/X_train_{args.img_size}_{args.class_name.lower()}.npy"):
+    print("Arrays not found, generating...")
+    preprocessor = PreprocessImages("F:/Datasets/NIH X-Rays/data",
+                                    args.img_size)
+    (X_train, y_train), (X_test, y_test) = preprocessor.start_single(args.class_name.capitalize())
+
+else:
+    X_train = np.load(open(f"data/arrays/X_train_{args.img_size}_{args.class_name.lower()}.npy", "rb"))
+    y_train = np.load(open(f"data/arrays/y_train_{args.img_size}_{args.class_name.lower()}.npy", "rb"))
+    X_test = np.load(open(f"data/arrays/X_test_{args.img_size}_{args.class_name.lower()}.npy", "rb"))
+    y_test = np.load(open(f"data/arrays/y_test_{args.img_size}_{args.class_name.lower()}.npy", "rb"))
+
+# Convert channels-last to channels-first format
+X_train = np.transpose(X_train, (0, 3, 1, 2))
+X_test = np.transpose(X_test, (0, 3, 1, 2))
+
+model = torch.hub.load('pytorch/vision:v0.6.0', 'densenet201',
+                       pretrained=False)
+model.features[0] = nn.Conv2d(1, 64, kernel_size=(7, 7), stride=(2, 2),
+                              padding=(3, 3), bias=False)
+model.classifier = nn.Sequential(
+    nn.Linear(in_features=1920, out_features=1, bias=True),
+    # nn.Sigmoid()
+)
+
+model.to(args.device)
+if args.checkpoint:
+    model.load_state_dict(torch.load(args.checkpoint))
+
+loss_fn = nn.BCEWithLogitsLoss().to(args.device)
+optimizer = Adamax(model.parameters(), lr=1e-6)
+scheduler = lr_scheduler.ReduceLROnPlateau(optimizer)
+mse_fn = nn.MSELoss()
+
+X_train = torch.Tensor(X_train)
+y_train = torch.Tensor(y_train)
+X_test = torch.Tensor(X_test)
+y_test = torch.Tensor(y_test)
+
+dataset = TensorDataset(X_train, y_train)
+train_set, val_set = random_split(dataset, [40000, 11376])
+test_set = TensorDataset(X_test, y_test)
+traindata = DataLoader(train_set, shuffle=True,
+                       pin_memory=args.pin_mem, batch_size=args.batch_size)
+valdata = DataLoader(val_set, shuffle=True,
+                     pin_memory=args.pin_mem, batch_size=args.batch_size)
+testdata = DataLoader(test_set, shuffle=True,
+                      pin_memory=args.pin_mem, batch_size=args.batch_size)
+
+best_model_wts = copy.deepcopy(model.state_dict())
+
+writer = SummaryWriter(f"data/logs/{args.name}-{int(starttime)}")
+dummy_input_shape = (1, 1, args.img_size, args.img_size)
+dummy_input = torch.randn(*dummy_input_shape, device=args.device)
+writer.add_graph(model, dummy_input)
+writer.flush()
+scaler = GradScaler()
+for epoch in range(args.epochs):
+    print(f"Epoch {epoch+1}/{args.epochs}")
+    print('='*61)
+
+    running_loss = 0.0
+    running_mse = 0.0
+    running_correct = 0
+    print('Training')
+    progressbar = tqdm(traindata, unit='steps', dynamic_ncols=True)
+    for index, (inputs, labels) in enumerate(progressbar):
+
+        inputs, labels = inputs.to(args.device), labels.to(args.device)
+
+        model.train()
+        optimizer.zero_grad()
+
+        with torch.set_grad_enabled(True):
+            with autocast():
+                outputs = model(inputs)
+                loss = loss_fn(outputs, labels)
+                mse = mse_fn(outputs, labels)
+            scaler.scale(loss).backward()
+            scaler.step(optimizer)
+            scaler.update()
+
+        running_loss += loss.item()
+        running_mse += mse.item()
+
+        outputs = torch.round(torch.sigmoid(outputs))
+        running_correct += (outputs == labels).sum().float()/args.batch_size
+        progressbar.set_description(f'Loss: {running_loss/(index+1):.5f}, \
+MSE: {running_mse/(index+1):.5f}, Accuracy: {running_correct/(index+1):.2%}')
+        progressbar.refresh()
+
+    epoch_loss = running_loss / len(traindata)
+    epoch_mse = running_mse / len(traindata)
+    epoch_accuracy = running_correct / len(traindata)
+
+    running_loss = 0.0
+    running_mse = 0.0
+    running_correct = 0
+
+    model.eval()
+    print('\nValidation')
+    progressbar = tqdm(valdata, unit='steps', dynamic_ncols=True)
+    for index, (inputs, labels) in enumerate(progressbar):
+
+        inputs, labels = inputs.to(args.device), labels.to(args.device)
+
+        with torch.no_grad():
+            with autocast():
+                outputs = model(inputs)
+                loss = loss_fn(outputs, labels)
+                mse = mse_fn(outputs, labels)
+
+        running_loss += loss.item()
+        running_mse += mse.item()
+        outputs = torch.round(torch.sigmoid(outputs))
+        running_correct += (outputs == labels).sum().float()/args.batch_size
+        progressbar.set_description(f'Val loss: {running_loss/(index+1):.5f}, \
+Val MSE: {running_mse/(index+1):.5f}, Accuracy: {running_correct/(index+1):.2%}')
+        progressbar.refresh()
+
+    val_loss = running_loss / len(valdata)
+    val_mse = running_mse / len(valdata)
+    val_accuracy = running_correct / len(traindata)
+    scheduler.step(val_mse)
+
+    if epoch == 0:
+        best_loss = val_loss
+        best_model_wts = copy.deepcopy(model.state_dict())
+        path = os.path.join(args.checkpoint_dir, 'best_weights.pth')
+        torch.save(best_model_wts, path)
+    elif val_loss < best_loss:
+        best_loss = val_loss
+        best_model_wts = copy.deepcopy(model.state_dict())
+        path = os.path.join(args.checkpoint_dir, 'best_weights.pth')
+        torch.save(best_model_wts, path)
+
+    accuracy_data = {'Training': epoch_accuracy*100, 'Validation': val_accuracy*100}
+    loss_data = {'Training': epoch_loss, 'Validation': val_loss}
+    mse_data = {'Training': epoch_mse, 'Validation': val_mse}
+    writer.add_scalars('Accuracy', accuracy_data, epoch+1)
+    writer.add_scalars('Loss', loss_data, epoch+1)
+    writer.add_scalars('MSE', mse_data, epoch+1)
+    writer.flush()
+
+    checkpoint_path = os.path.join(args.checkpoint_dir,
+                                   f"checkpoint-{epoch:03d}.pth")
+    torch.save(model.state_dict(), checkpoint_path)
+    print(f"Checkpoint saved to checkpoint-{epoch:03d}.pth\n")
+
+writer.close()
+
+time_elapsed = time.time() - starttime
+time_hours = int(time_elapsed // 3600)
+time_minutes = int((time_elapsed-time_hours) // 60)
+time_seconds = round(time_elapsed % 60)
+print(f"Training complete in {time_hours}h \
+{time_minutes}m {time_seconds}s")
+
+model.load_state_dict(best_model_wts)
+model.eval()
+running_loss = 0.0
+running_mse = 0.0
+running_correct = 0
+
+print('Testing')
+progressbar = tqdm(testdata, dynamic_ncols=True)
+for index, (inputs, labels) in enumerate(progressbar):
+
+    inputs, labels = inputs.to(args.device), labels.to(args.device)
+
+    with torch.no_grad():
+        with autocast():
+            outputs = model(inputs)
+            loss = loss_fn(outputs, labels)
+            mse = mse_fn(outputs, labels)
+
+    running_loss += loss.item()
+    running_mse += mse.item()
+    outputs = torch.round(torch.sigmoid(outputs))
+    running_correct += (outputs == labels).sum().float()/args.batch_size
+    progressbar.set_description(f'Test loss: {running_loss/(index+1):.5f}, \
+Test MSE: {running_mse/(index+1):.5f}, Accuracy: {running_correct/(index+1):.2%}')
+    progressbar.refresh()
+
+print("Saving model weights")
+savepath = f"data/models/{args.name}-{int(starttime)}.pth"
+savepath_weights = f"data/models/{args.name}-{int(starttime)}_weights.pth"
+torch.save(model.state_dict(), savepath_weights)
+torch.save(model, savepath)
+print("Model saved!\n")
--- multithreaded_preprocessing.py
+++ multithreaded_preprocessing.py
@@ -2,9 +2,10 @@ import multiprocessing as mp
 import os
 import random
 import time
+from functools import partial
 
-import numpy as np
 import cv2
+import numpy as np
 import pandas as pd
 from sklearn.preprocessing import MultiLabelBinarizer
 
@@ -84,6 +85,57 @@ class PreprocessImages:
             if name in files:
                 return os.path.join(root, name)
 
+    def _preprocess_single(self, classname: str, row: pd.Series) -> tuple:
+        """Preprocesses a single row of data
+
+        Args:
+            row (pd.Series): Row of Pandas dataframe
+
+        Returns:
+            training_data: If data is in train_list, contains np.ndarray
+                with image data and list of labels
+            testing_data: If data is in test_list, contains np.ndarray
+                with image data and list of labels
+        """
+        row = row[1]
+        imagename = row['Image Index']
+        label = row['Finding Labels']
+
+        training_data = []
+        testing_data = []
+
+        imagesize_tuple = (self.image_size, self.image_size)
+
+        if imagename in self.train_list:
+            print("Training data: ", imagename)
+            imagepath = self._findimage(imagename, self.dataset_dir)
+
+            img_array = cv2.imread(imagepath, cv2.IMREAD_GRAYSCALE)
+            img_array = cv2.resize(img_array, imagesize_tuple)
+
+            if label == 'No Finding':
+                label_binary = [0]
+            elif label == classname:
+                label_binary = [1]
+
+            training_data = [img_array, label_binary]
+
+        elif imagename in self.test_list:
+            print("Testing data: ", imagename)
+            imagepath = self._findimage(imagename, self.dataset_dir)
+
+            img_array = cv2.imread(imagepath, cv2.IMREAD_GRAYSCALE)
+            img_array = cv2.resize(img_array, imagesize_tuple)
+
+            if label == 'No Finding':
+                label_binary = [0]
+            elif label == classname:
+                label_binary = [1]
+
+            testing_data = [img_array, label_binary]
+
+        return training_data, testing_data
+
     def _preprocess(self, row: pd.Series) -> tuple:
         """Preprocesses a single row of data
 
@@ -128,6 +180,39 @@ class PreprocessImages:
             testing_data = [img_array, multilabels]
 
         return training_data, testing_data
+
+    def start_single(self, classname: str):
+
+        df = self.csv_data[self.csv_data['Finding Labels'].isin([classname, "No Finding"])]
+        starttime = time.time()
+        pool = mp.Pool(mp.cpu_count())
+        results = pool.map(partial(self._preprocess_single, classname), df.iterrows())
+        training_data = [result[0] for result in results if result[0] != []]
+        testing_data = [result[1] for result in results if result[1] != []]
+
+        print("Combining outputs...")
+
+        random.shuffle(training_data)
+        random.shuffle(testing_data)
+
+        X_train = [item[0] for item in training_data]
+        y_train = [item[1] for item in training_data]
+        X_test = [item[0] for item in testing_data]
+        y_test = [item[1] for item in testing_data]
+
+        X_train = np.array(X_train).reshape(self.resizetuple)
+        X_test = np.array(X_test).reshape(self.resizetuple)
+
+        elapsed_time = time.time() - starttime
+        print(f"Time taken: {elapsed_time // 60}m {elapsed_time % 60}s")
+
+        print("Saving Arrays")
+        np.save(f"data/arrays/X_train_{self.image_size}_{classname.lower()}.npy", X_train)
+        np.save(f"data/arrays/y_train_{self.image_size}_{classname.lower()}.npy", y_train)
+        np.save(f"data/arrays/X_test_{self.image_size}_{classname.lower()}.npy", X_test)
+        np.save(f"data/arrays/y_test_{self.image_size}_{classname.lower()}.npy", y_test)
+
+        return (X_train, y_train), (X_test, y_test)
 
     def start(self):
         """
